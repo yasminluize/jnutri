27d8d5459da4be677357958b1b14b04c0f327cbc
yasminluize <60760457+yasminluize@users.noreply.github.com>
2020-02-20 21:20:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+let pacientes = document.querySelectorAll(".paciente");
+
+pacientes.forEach(paciente => {
+    let peso = paciente.querySelector(".info-peso").textContent;
+    let altura = paciente.querySelector(".info-altura").textContent;
+    
+    let pesoValido = true;
+    let alturaValida = true;
+    
+    let tdIMC = paciente.querySelector(".info-imc");
+    
+    if (peso <= 0 || peso > 1000) {
+        pesoValido = false;
+        tdIMC.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+    if (altura <= 0) {
+        alturaValida = false;
+        tdIMC.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if (pesoValido && alturaValida) {
+        let imc = peso / altura ** 2;
+        tdIMC.textContent = imc.toFixed(2);
+    }
+});
+
+let titulo = document.querySelector("#titulo");
+titulo.onclick = mostrarMensagem; 
+function mostrarMensagem(){  
+console.log("evento aconteceu");
+
+}
+function trocarCor(){
+    let cor = parseInt(Math.random()*255);
+    titulo.style.color = "rgb(" +cor+ " ," +cor+ " , " +cor+ ")"; 
+}
+    
